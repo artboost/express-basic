a7d37cebf296428067081096a0dfb4c065745936
aksel <aksel@akseltorgard.com>
2018-10-18 18:23:00 +0200

    Added documentation for routes in router.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,22 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('hello world'));
 
+
+/**
+ * /entries
+ *    GET  -> All entries
+ *    POST -> Create new entry
+ *      Body: { message }
+ */
 router.use('/entries', entries);
+
+/**
+ * /entry/:id
+ *    GET
+ *    POST -> Update
+ *      Body: { message }
+ *    DELETE
+ */
 router.use('/entry', entry);
 
 module.exports = router;
