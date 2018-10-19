65770da2e3445ac7b9e29003d986ac6b0d92b6f9
aksel <aksel@akseltorgard.com>
2018-10-19 11:38:27 +0200

    Added second error handler, which handles internal server errors.
    Disabled prefer promise reject

--- src/app.js
+++ src/app.js
@@ -13,6 +13,17 @@ app.use(express.json());
 
 app.use(router);
 
+app.use((err, req, res, next) => {
+  if (err instanceof Error) {
+    console.error(err);
+    next({
+      message: err.toString(),
+      status: 500,
+    });
+  } else {
+    next(err);
+  }
+});
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => res.status(err.status || 500).json({ error: err }));
 
