c4e7be0b7d593aab56073c7785b11d619a3542a6
aksel <aksel@akseltorgard.com>
2018-10-19 11:39:20 +0200

    Moved middlewares into indes.js, added async executor.

---
+++ src/middleware/index.js
@@ -0,0 +1,23 @@
+// For use with async functions, negating mandatory use of try/catch.
+const executeAsync = fn => (req, res, next) => {
+  Promise
+    .resolve(fn(req, res, next))
+    .catch(next);
+};
+
+const validate = (object = {}, required, next) => {
+  const keys = Object.keys(object);
+
+  const missing = required.filter(column => keys.indexOf(column) === -1);
+  if (missing.length > 0) {
+    next({ status: 400, message: `Invalid body. Missing: ${missing}` });
+  } else {
+    next();
+  }
+};
+
+module.exports.executeAsync = executeAsync;
+module.exports.validate = {
+  body: required => (req, res, next) => validate(req.body, required, next),
+  query: required => (req, res, next) => validate(req.query, required, next),
+};
