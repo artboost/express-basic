ca89081a7feb8baf499af00a6cdd57b70db92cf8
aksel <aksel@akseltorgard.com>
2018-10-19 11:35:29 +0200

    A bunch of stuff.
    * Upgraded packages
    
    * Removed body-parser; use express.json() instead.
    
    * Disable lint rule no-console; irrelevant for node.
    
    * Removed graceful shutdown stuff.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const router = require('./routes');
 
@@ -10,22 +9,11 @@ const app = express();
 
 // MIDDLEWARE
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(router);
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => res.status(err.status || 500).json({ error: err }));
 
-const server = app.listen(PORT);
-
-const gracefulShutdown = () => {
-  server.close(() => process.exit());
-  setTimeout(() => process.exit(), 10000);
-};
-
-// listen for TERM signal .e.g. kill
-process.on('SIGTERM', gracefulShutdown);
-
-// listen for INT signal e.g. Ctrl-C
-process.on('SIGINT', gracefulShutdown);
+app.listen(PORT, () => console.log(`listening @ localhost:${PORT}`));
