91599483c83ee64e31db433cad9492166d7d1d8b
aksel <aksel@akseltorgard.com>
2018-10-23 13:06:50 +0200

    Added unauthorized and internal server error.

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,14 +1,14 @@
-class NotFoundError extends Error {
-  constructor(m = 'Not found.') {
+class IllegalArgumentException extends Error {
+  constructor(m = 'IllegalArgument') {
     super(m);
-    this.status = 404;
+    this.status = 400;
   }
 }
 
-class IllegalArgumentException extends Error {
-  constructor(m = 'Illegal argument.') {
+class UnauthorizedError extends Error {
+  constructor(m = 'Unauthorized') {
     super(m);
-    this.status = 400;
+    this.status = 401;
   }
 }
 
@@ -19,8 +19,24 @@ class ForbiddenError extends Error {
   }
 }
 
+class NotFoundError extends Error {
+  constructor(m = 'NotFound') {
+    super(m);
+    this.status = 404;
+  }
+}
+
+class InternalServerError extends Error {
+  constructor(m = 'Internal server error') {
+    super(m);
+    this.status = 500;
+  }
+}
+
 module.exports = {
-  NotFoundError,
   IllegalArgumentException,
+  UnauthorizedError,
   ForbiddenError,
+  NotFoundError,
+  InternalServerError,
 };
