8387ceef367b0199062e0d99afa01c3b81da3f54
aksel <aksel@akseltorgard.com>
2018-10-25 11:23:13 +0200

    Use Error objects for auth errors.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
+const {
+  UnauthorizedError,
+  ForbiddenError,
+} = require('../errors');
+
 const getKey = (header, callback) => {
   const keyPath = process.env.KEY_PATH || 'https://example.org/key.pub';
   axios.get(keyPath)
@@ -17,7 +22,7 @@ const authMiddleware = (required = false, adminRequired = false) => (req, res, n
   const authString = req.get('authorization');
   if (!authString) {
     if (required) {
-      next({ status: 401 });
+      next(new UnauthorizedError());
     } else {
       res.locals.authChecked = true;
       next();
@@ -29,20 +34,14 @@ const authMiddleware = (required = false, adminRequired = false) => (req, res, n
 
   jwt.verify(token, getKey, { algorithm: ['RS256'] }, (err, decoded) => {
     if (err) {
-      next({
-        status: 401,
-        message: err.message,
-      });
+      next(new UnauthorizedError(err.message));
       return;
     }
 
     const { user } = decoded;
 
     if (adminRequired && !user.is_admin) {
-      next({
-        status: 403,
-        message: 'not admin',
-      });
+      next(new ForbiddenError('Not admin'));
       return;
     }
 
